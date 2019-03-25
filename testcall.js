587bae7a77cc90b7cb7e0ed08d2d1cb9859079f6
Ramisa Murshed <35822743+ramisamurshed@users.noreply.github.com>
2019-03-24 23:03:25 -0400

    oops

--- testcall.js
+++ testcall.js
@@ -1,5 +1,5 @@
-const accountSid = 'ACa154621e8d0302fffa0ce8422ec3ffa9';
-const authToken = '2c0f9b5ffdca73c9e6467e7cb4e40344';
+const accountSid = 'AXXXXXXXXXXXX';
+const authToken = 'XXXXXXXXXXXXXXXXXX';
 const client = require('twilio')(accountSid, authToken);
 
 client.calls
